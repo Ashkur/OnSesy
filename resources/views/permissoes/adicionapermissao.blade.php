bd9b2ded97600e15e43a930ace9fdd3b7913cc77
Denym Andrade <denymrr@hotmail.com>
2018-01-31 16:24:01 -0400

    botoes

--- resources/views/permissoes/adicionapermissao.blade.php
+++ resources/views/permissoes/adicionapermissao.blade.php
@@ -5,12 +5,13 @@
     <div class="row">
         <div class="col-md-8 col-md-offset-2">
             
-            <table class="table">
+            <table class="class="table table-hover"">
                 <thead>
                     <tr>
                     <th scope="col">#</th>
                     <th scope="col">nome</th>
                     <th scope="col">descricao</th>
+                    <th scope="col">Ações</th>
                     </tr>
                 </thead>
                 <tbody>
@@ -20,6 +21,9 @@
                     <th scope="row">{{$permissao->id}}</th>
                     <td>{{$permissao->nome}}</td>
                     <td>{{$permissao->descricao}}</td>
+                    <td><a link="#" class="btn btn-warning ">Editar</a>
+                        <a link="#" class="btn btn-warning ">Excluir</a>
+                    </td>
                     </tr>
                     @endforeach
                     
