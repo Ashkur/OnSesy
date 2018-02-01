3635bbd658939427c307109b22d940fb9a31ff03
DenymAndrade <denymrr@hotmail.com>
2018-02-01 17:59:11 -0400

    css

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -6,24 +6,38 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
 
     <!-- bootstrap cdn -->
+    <!--
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
 <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
 
+
+    <link rel="stylesheet" href="../css/bootstrap/compiler/bootstrap.css">
+    <link rel="stylesheet" href="../css/bootstrap/compiler/style.css">
+  -->
     <!-- CSRF Token -->
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
     <title>{{ config('app.name', 'Laravel') }}</title>
 
     <!-- Styles -->
-    <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+    <link href="{{ asset('css/style.css') }}" rel="stylesheet">
 </head>
 <body>
+    <!--<script type="text/javascript" src="../js/jquery/dist/jquery.js"></script>
+    <script type="text/javascript" src="../js/jquery/dist/app.js"></script>
+    <script type="text/javascript" src="../js/jquery/dist/jquery-3.1.1.min.js"></script>
+    <script type="text/javascript" src="../js/popper.js/dist/umd/popper.js"></script>
+    <script type="text/javascript" src="../js/bootstrap/dist/js/bootstrap.js"></script>
+
 <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
 <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
+-->
+
+<script type="text/javascript" src="{{ asset('js/apps.js') }}"></script>
     <div id="app">
     <nav class="navbar navbar-expand-lg navbar-dark bg-gradient-dark" >
     <div class="container">
-        <img src="node_modules/bootstrap/images/bootstrap-solid.svg" alt="Sistema Online de Seletivos" width="30" height="30" class="d-inline-block align-top mr-2" alt="">
+        <img src="../images/bootstrap-solid.svg" alt="Sistema Online de Seletivos" width="30" height="30" class="d-inline-block align-top mr-2" alt="">
       <a class="navbar-brand h1 mb-0" href="#" alt="Sistema Online de Seletivos">OnSeSy</a>
         <button  class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSite">
           <span class="navbar-toggler-icon"></span>
--- webpack.mix.js
+++ webpack.mix.js
@@ -9,7 +9,19 @@ let mix = require('laravel-mix');
  | for your Laravel application. By default, we are compiling the Sass
  | file for the application as well as bundling up all the JS files.
  |
- */
+ 
 
 mix.js('resources/assets/js/app.js', 'public/js')
    .sass('resources/assets/sass/app.scss', 'public/css');
+
+*/
+mix.combine('assets/bootstrap/compiler/bootstrap.css',
+            'assets/bootstrap/compiler/style.css'
+, 'public/css/style.css');
+
+mix.combine('assets/js/popper.js/dist/umd/popper.js',
+            'assets/js/bootstrap.js',
+            'assets/js/jquery/dist/jquery.js',
+            'assets/js/jquery/dist/jquery-3.1.1.min.js',
+            'assets/js/jquery/dist/app.js'
+  , 'public/js/apps.js');
