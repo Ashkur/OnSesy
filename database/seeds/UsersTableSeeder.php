d42c51d40fd4b3dff51f80cf8d286e8d3699be4b
Uriel Carneiro da Silva <urielhk17@gmail.com>
2018-02-05 15:27:59 -0400

    correção seed da tabela Users

--- database/seeds/UsersTableSeeder.php
+++ database/seeds/UsersTableSeeder.php
@@ -13,18 +13,21 @@ class UsersTableSeeder extends Seeder
     {
         DB::table('users')->insert([
             'name' => 'administrador',
+            'cpf' => '111.111.111.-11',
             'email' => 'administrador@gmail.com',
             'password' => bcrypt('secret'),
         ]);
 
         DB::table('users')->insert([
             'name' => 'Auxiliar',
+            'cpf' => '222.222.222.-22',
             'email' => 'auxiliar@gmail.com',
             'password' => bcrypt('secret'),
         ]);
 
         DB::table('users')->insert([
             'name' => 'SubAuxiliar',
+            'cpf' => '333.333.333.-33',
             'email' => 'subauxiliar@gmail.com',
             'password' => bcrypt('secret'),
         ]);
