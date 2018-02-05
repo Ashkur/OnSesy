5db59d91b1cc3f8bc51c55f14e860d97d2901396
Denym Andrade <denymrr@hotmail.com>
2018-02-05 17:31:21 -0400

    template

--- webpack.mix.js
+++ webpack.mix.js
@@ -11,27 +11,22 @@ let mix = require('laravel-mix');
  |
  
 
-mix.js('resources/assets/js/app.js', 'public/js')
-   .sass('resources/assets/sass/app.scss', 'public/css');
+
 
 */
 
-mix.combine(['resources/assets/js/materialize.js'], 'public/js/app.js');
+mix.combine(['resources/assets/js/materialize.js',
+            
+
+], 'public/js/app.js');
+
 
 mix.combine(['resources/assets/css/materialize.css',
-              'resources/assets/css/estilo.css',
+              'resources/assets/css/estilo.css'/*,
+              'resources/assets/css/bootstrap.min.css',
+              'resources/assets/css/demo.css',
+              'resources/assets/css/material-dashboard.css'*/
             ], 
               'public/css/app.css');
 
 
-/*
-mix.combine('assets/bootstrap/compiler/bootstrap.css',
-            'assets/bootstrap/compiler/style.css'
-, 'public/css/style.css');
-
-mix.combine('assets/js/popper.js/dist/umd/popper.js',
-            'assets/js/bootstrap.js',
-            'assets/js/jquery/dist/jquery.js',
-            'assets/js/jquery/dist/jquery-3.1.1.min.js',
-            'assets/js/jquery/dist/app.js'
-  , 'public/js/apps.js'); */
