036a413c3c4ace403c6d56ce2d2a855be3cd1619
AkyamaDsouza <akyama_ng@hotmail.com>
2018-02-06 09:06:56 -0400

    Deletando jquery kkkk

--- webpack.mix.js
+++ webpack.mix.js
@@ -14,30 +14,12 @@ let mix = require('laravel-mix');
 
 
 */
-/* Back */
-mix.combine(['resources/assets/js/materialize.js',
-            
 
+mix.combine([
+  'resources/assets/css/bootstrap.css',
+], 'public/css/app.css');
+
+mix.combine([
+  'resources/assets/js/jquery-3.3.1.min.js',
+  'resources/assets/js/bootstrap-min.js',
 ], 'public/js/app.js');
-
-
-mix.combine(['resources/assets/css/materialize.css',
-              'resources/assets/css/estilo.css',
-              'resources/assets/css/bootstrap.min.css',
-              'resources/assets/css/demo.css',
-              'resources/assets/css/material-dashboard.css'
-            ], 
-              'public/css/app.css');
-
-
-/* front */
-mix.combine(['resources/assets/bootstrap/compiler/bootstrap.css',
-            'resources/assets/bootstrap/compiler/style.css']
-, 'public/css/style.css');
-
-mix.combine(['resources/assets/js/popper.js/dist/umd/popper.js',
-            'resources/assets/js/bootstrap.js',
-            'resources/assets/js/jquery/dist/jquery.js',
-            'resources/assets/js/jquery/dist/jquery-3.1.1.min.js',
-            'resources/assets/js/jquery/dist/app.js']
-  , 'public/js/apps.js');
