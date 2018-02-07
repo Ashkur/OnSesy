b82148d89dadd658c0b9aa6318184cadaf92204c
AkyamaDsouza <akyama_ng@hotmail.com>
2018-02-07 10:23:54 -0400

    Correção Login

--- webpack.mix.js
+++ webpack.mix.js
@@ -15,31 +15,17 @@ let mix = require('laravel-mix');
 
 */
 
-/* sistema Site */
-mix.combine([
-  'resources/assets/css/bootstrap.css',
-  'resources/assets/css/bootstrap.min.css',
-  'resources/assets/css/main.css',
-], 'public/css/style.css');
-
-mix.combine([
-  'resources/assets/js/jquery/dist/jquery.js',
-  'resources/assets/js/jquery/dist/app.js',
-  'resources/assets/js/jquery/dist/jquery-3.1.1.min.js',
-  'resources/assets/js/popper.js/dist/umd/popper.js',
-  'resources/assets/bootstrap/dist/js/bootstrap.js',
-  
-], 'public/js/apps.js');
-
-
-
-
-/* sistema administrativo */
 mix.combine([
   'resources/assets/css/bootstrap.css',
 ], 'public/css/app.css');
 
 mix.combine([
+  'resources/assets/css/main.css',
+  'resources/assets/css/util.css',
+], 'public/css/login.css');
+
+mix.combine([
   'resources/assets/js/jquery-3.3.1.min.js',
   'resources/assets/js/bootstrap-min.js',
 ], 'public/js/app.js');
+
