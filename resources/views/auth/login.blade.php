f7e77116d4096da898011eb4acac08a067ac0fdf
AkyamaDsouza <akyama_ng@hotmail.com>
2018-02-08 15:41:07 -0400

    Melhorando o Login

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -4,13 +4,29 @@
 <div class="container">
 	<div class="row">
 		<div class="col-6 col-md-4 mx-auto">
+			@if (session('status'))
+				<div class="card">
+					{{ session('status') }}
+				</div>
+			@endif
 			<div class="card">
 				<div class="card-body">
-					<form class="form-signin">
+					<form class="form-signin" method="POST" action="{{ url('/login') }}">
+						{{ csrf_field() }}
+						<input type="email" id="inputEmail" value="{{ old('email') }}" class="form-control" placeholder="Email address" required autofocus>
 						<label for="inputEmail" class="sr-only">Email address</label>
-						<input type="email" id="inputEmail" class="form-control" placeholder="Email address" required autofocus>
-						<label for="inputPassword" class="sr-only">Password</label>
+						@if ($errors->has('email'))
+							<span>
+								<strong>{{ $errors->first('email') }}</strong>
+							</span>
+						@endif
 						<input type="password" id="inputPassword" class="form-control" placeholder="Password" required>
+						<label for="inputPassword" class="sr-only">Password</label>
+						@if ($errors->has('password'))
+							<span>
+								<strong>{{ $errors->first('password') }}</strong>
+							</span>
+						@endif
 						<div class="checkbox">
 						<label>
 							<input type="checkbox" value="remember-me"> Remember me
--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -6,15 +6,36 @@
 		<div class="col-6 col-md-4 mx-auto">
 			<div class="card">
 				<div class="card-body">
-					<form class="form-signin">
-					   <label for="inputEmail" class="sr-only">Nome</label>
-						<input type="text" id="inputEmail" class="form-control" placeholder="Nome" required autofocus>
+					<form class="form-signin" action="{{ url('/register') }}">
+						{{ csrf_field() }}
+						<input type="text" name="name" id="inputNome" class="form-control" placeholder="Nome" required autofocus>
+						<label for="inputNome" class="sr-only">Nome</label>
+						@if ($errors->has('name'))
+							<span>
+								<strong>{{ $errors->first('name') }}</strong>
+							</span>
+						@endif
+						<input type="email" name="email"  id="inputEmail" class="form-control" placeholder="Email" required autofocus>
 						<label for="inputEmail" class="sr-only">Email</label>
-						<input type="email" id="inputEmail" class="form-control" placeholder="Email" required autofocus>
+						@if ($errors->has('email'))
+							<span>
+								<strong>{{ $errors->first('email') }}</strong>
+							</span>
+						@endif
+						<input type="password" name="password" id="inputPassword" class="form-control" placeholder="Senha" required>
 						<label for="inputPassword" class="sr-only">Senha</label>
-						<input type="password" id="inputPassword" class="form-control" placeholder="Senha" required>
-						<label for="inputPassword" class="sr-only">Confirme a senha</label>
-						<input type="password" id="inputPassword" class="form-control" placeholder="Confirme a senha" required>
+						@if ($errors->has('password'))
+							<span>
+								<strong>{{ $errors->first('password') }}</strong>
+							</span>
+						@endif
+						<label for="inputPasswordConfirmation" name="password_confirmation" class="sr-only">Confirme a senha</label>
+						<input type="password" id="inputPasswordConfirmation" class="form-control" placeholder="Confirme a senha" required>
+						@if ($errors->has('password_confirmation'))
+							<span>
+								<strong>{{ $errors->first('password_confirmation') }}</strong>
+							</span>
+						@endif
 						</div>
 						<button class="btn btn-lg btn-primary btn-block" type="submit">Sign in</button>
 					</form>
