d796e171a3d19b949a179881e61162476aa27bc5
GabrielSouzaSi <gos.gabri@gmail.com>
2018-03-09 16:32:53 -0400

    novo login

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,42 +1,51 @@
 @extends('layouts.app')
 
 @section('content')
-<div class="container">
-	<div class="row">
-		<div class="col-6 col-md-4 mx-auto">
-			@if (session('status'))
+<div class="container py-5">
+        <div class="col-md-12">
+            <div class="col-md-5 mx-auto">
+		@if (session('status'))
 				<div class="card">
 					{{ session('status') }}
 				</div>
-			@endif
-			<div class="card">
-				<div class="card-body">
-					<form class="form-signin" method="POST" action="{{ url('/login') }}">
-						{{ csrf_field() }}
-						<input type="text" id="inputEmail" value="{{ old('email') }}" class="form-control" name="email" placeholder="Email" autofocus>
-						<label for="inputEmail" class="sr-only">E-mail</label>
-						@if ($errors->has('email'))
+					@endif
+                    <!-- form card login -->
+                    <div class="card rounded-0">
+                        <div class="card-header">
+                            <h3 class="mb-0">Login</h3>
+                        </div>
+                        <div class="card-body">
+                            <form class="form-signin" method="POST" action="{{ url('/login') }}">
+								{{ csrf_field() }}
+                                <div class="form-group">
+                                    <label for="inputEmail">E-mail</label>
+                                    <input type="text" id="inputEmail" value="{{ old('email') }}" class="form-control" name="email" autofocus>
+									@if ($errors->has('email'))
 							<span>
 								<strong>{{ $errors->first('email') }}</strong>
 							</span>
-						@endif
-						<input type="password" id="inputPassword" class="form-control" name="password" placeholder="Senha">
-						<label for="inputPassword" class="sr-only">Senha</label>
-						@if ($errors->has('password'))
+									@endif
+                                </div>
+                                <div class="form-group">
+                                    <label for="inputPassword">Senha</label>
+                                    <input type="password" id="inputPassword" class="form-control" name="password" placeholder="Senha">
+									@if ($errors->has('password'))
 							<span>
 								<strong>{{ $errors->first('password') }}</strong>
 							</span>
-						@endif
-						<div class="checkbox">
+									@endif
+                                </div>
+                                <div class="checkbox">
 						<label>
-							<input type="checkbox" value="remember-me"> Remember me
+							<input type="checkbox" value="remember-me"> Lembrar-me
 						</label>
 						</div>
-						<button class="btn btn-lg btn-primary btn-block" type="submit">Sign in</button>
-					</form>
-				</div>
-			</div>
+                                <button type="submit" class="btn btn-success btn-lg float-right" id="btnLogin">Login</button>
+                            </form>
+                        </div>
+                        <!--/card-block-->
+                    </div>
 		</div>
-	</div>
 </div> <!-- /container -->
+</div>
 @endsection
