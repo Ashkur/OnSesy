d7091b4bf32b92260189033eee27b63f0d24a316
GabrielSouzaSi <gos.gabri@gmail.com>
2018-03-23 17:38:19 -0400

    show

--- resources/views/candidato/comprovante.blade.php
+++ resources/views/candidato/comprovante.blade.php
@@ -5,7 +5,7 @@
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta name="viewport" content="width=device-width, initial-scale=1", "shrink-to-fit=no">
         <meta name="csrf-token" content="{{ csrf_token() }}">
-        <title>@yield('title') OnSeSy - Sistema Online de Seletivos</title>
+        <title>OnSeSy - Sistema Online de Seletivos</title>
         <!-- Fonts -->
         <link href="https://fonts.googleapis.com/css?family=Raleway:100,600" rel="stylesheet" type="text/css">
         <!-- Styles -->
